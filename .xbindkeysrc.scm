32f4d902897a69f38aeac3d5667f096fe1fa47eb
nako08 <nako08@protonmail.com>
2020-04-18 16:21:09 +0200

    improved version

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,11 +1,17 @@
 ;;   This configuration is guile based.
 ;;   http://www.gnu.org/software/guile/guile.html
-;;   This config script us supposed to live in the homedirectory.
-;;   This was a not so quick but probably yet dirty hack by Zero Angel
+;;   This config script is supposed to live in the homedirectory.
+;;   Awesome script created by Zero Angel
+;;   This couldnt have been possible without seeing Vee Lee's configuration file
 ;;   You'll need xdotool and xbindkeys with -guile support compiled for this to work (The Ubuntu xbindkeys will have this support by default).
-;;   This is heavily based on Vee Lee's configuration file
 ;;   It assigns keybindings to the scroll wheel  on the fly when mouse modifier keys are pressed. Useful for mice with lots of buttons!
+;;   v1.0 -- Shoulder button + scrollwheel bindings
+;;   v1.1 -- Fixes some 'stuckness' problems with the modifer keys (ctrl, alt, shift)
+;;   v1.2 -- Can trigger events properly if the modifier button is simply pressed and released by itself. Forcefully clears modifier keys when the shoulder buttons are depressed.
 ;;	 * Found on: https://www.linuxquestions.org/questions/linux-desktop-74/%5Bxbindkeys%5D-advanced-mouse-binds-4175428297/#post4785538
+
+
+(define actionperformed 0)
 
 (define (first-binding)
 "First binding"
@@ -20,6 +26,11 @@
 "reset first binding"
 (ungrab-all-keys)
 (remove-all-keys)
+;; Set Action Performed state back to 0
+(set! actionperformed 0)
+;; Forcefully release all modifier keys!
+(run-command "xdotool keyup ctrl keyup alt keyup shift keyup super&")
+
 (first-binding)
 (grab-all-keys))
 
@@ -32,17 +43,22 @@
 ;; Scroll Up
 (xbindkey-function '("b:4")
                 (lambda ()
-                   (run-command "xdotool key --clearmodifiers ctrl+alt+Up&")
+;; Emulate Ctrl+Alt+Up (Workspace Up)
+                (run-command "xdotool keydown ctrl keydown alt key Up keyup ctrl keyup alt&")
+		(set! actionperformed 1)
 ))
 
 ;; Scroll Down
 (xbindkey-function '("b:5")
                 (lambda ()
-                  (run-command "xdotool key --clearmodifiers ctrl+alt+Down&")
+;; Emulate Ctrl+Alt+Down (Workspace Down)
+                (run-command "xdotool keydown ctrl keydown alt key Down keyup ctrl keyup alt&")
+		(set! actionperformed 1)
 ))
 
 (xbindkey-function '(release "b:9") (lambda ()
- (run-command "echo -e 'Placeholder for button release command' &")
+;; Perform Action if Button 8 is pressed and released by itself
+(if (= actionperformed 0) (run-command "zenity --info --title=hi --text=Button9ReleaseEvent &"))
 (reset-first-binding)))
 (grab-all-keys))
 
@@ -54,30 +70,28 @@
 ;; Scroll Up
 (xbindkey-function '("b:4")
                 (lambda ()
-                   (run-command "xdotool key --clearmodifiers alt+shift+tab&")
+;; Emulate Alt+Shift+Tab (previous window)
+                (run-command "xdotool keydown alt keydown shift key Tab keyup alt keyup shift")
+		(set! actionperformed 1)
 ))
 
 ;; Scroll Down
 (xbindkey-function '("b:5")
                 (lambda ()
-                  (run-command "xdotool key --clearmodifiers alt+tab&")
+;; Emulate Alt+Tab (next window)
+                (run-command "xdotool keydown alt key Tab keyup alt")
+		(set! actionperformed 1)
 ))
 
 (xbindkey-function '(release "b:8") (lambda ()
- (run-command "echo -e 'Placeholder for button release command'&")
-(reset-first-binding)))
-(grab-all-keys))
-
-;; (define (scroll-apps-binding)
-;; "scroll-apps-binding"
-;; (ungrab-all-keys)
-;; (remove-all-keys)
-;; (xbindkey-function '("b:4")(run-command "echo -e 'KeyStrPress Tab\nKeyStrRelease Tab' | xmacroplay :0 &"))
-;; (xbindkey-function '("b:5")(run-command "echo -e 'KeyStrPress Shift_L\nKeyStrPress Tab\nKeyStrRelease Tab\nKeyStrRelease Shift_L' | xmacroplay :0 &"))
-;; (xbindkey-function '("b:1") reset-first-binding)
-;; (grab-all-keys))
-;; (debug)
+;; Perform Action if Button 8 is pressed and released by itself
+     (if (= actionperformed 0) (run-command "zenity --info --title=hi --text=Button8ReleaseEvent &"))
+(reset-first-binding)
+))
 (grab-all-keys)
+)
+
+;; (debug)
 (first-binding)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
